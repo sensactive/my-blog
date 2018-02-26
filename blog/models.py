e686c175fa22cd6e61bf20b6978509236e14bd1e
sensactive <sensor14@list.ru>
2018-02-26 17:41:38 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.utils import timezone
+from PIL import Image
+from os import path
+
+
+_MAX_SIZE = 300
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    img = models.ImageField(blank=True, upload_to='images/%Y/%m/%d', verbose_name='Картинка')
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def save(self, *args, **kwargs):
+        # Сначала - обычное сохранение
+        super(Post, self).save(*args, **kwargs)
+
+        # Проверяем, указан ли логотип
+        if self.img:
+            filename = self.img.path
+            width = self.img.width
+            height = self.img.height
+
+            max_size = max(width, height)
+
+            # Может, и не надо ничего менять?
+            if max_size > _MAX_SIZE:
+                # Надо, Федя, надо
+                image = Image.open(filename)
+                # resize - безопасная функция, она создаёт новый объект, а не
+                # вносит изменения в исходный, поэтому так
+                image = image.resize(
+                    (round(width / max_size * _MAX_SIZE),  # Сохраняем пропорции
+                     round(height / max_size * _MAX_SIZE)),
+                    Image.ANTIALIAS
+                )
+                # И не забыть сохраниться
+                image.save(filename)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
